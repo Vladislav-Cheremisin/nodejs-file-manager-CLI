eafbc7b98cb8cfa0e2f016c871860c4966783f1e
Vladislav Cheremisin <wowjurneo@list.ru>
2022-06-12 13:04:53 +0300

    feat: complete os operations

--- src/osOperations.js
+++ src/osOperations.js
@@ -19,6 +19,41 @@ class OSOperations {
             this.writable.write(`${JSON.stringify(os.EOL)}\n`);
             dirData.showDirInfo();
             break;
+          case "--cpus":
+            const cpusInfo = os.cpus();
+            const result = [];
+
+            cpusInfo.forEach((cp) => {
+              let speedGHZ = null;
+
+              if (cp.speed < 100) {
+                speedGHZ = cp.speed / 10;
+              } else {
+                speedGHZ = cp.speed / 1000;
+              }
+
+              result.push({
+                model: cp.model.split("@")[0].trim(),
+                speed: +speedGHZ.toFixed(1),
+              });
+            });
+
+            console.log(result);
+            dirData.showDirInfo();
+            break;
+          case "--homedir":
+            this.writable.write(`${os.homedir()}\n`);
+            dirData.showDirInfo();
+            break;
+          case "--username":
+            const username = os.userInfo().username;
+            this.writable.write(`${username}\n`);
+            dirData.showDirInfo();
+            break;
+          case "--architecture":
+            this.writable.write(`${os.arch()}\n`);
+            dirData.showDirInfo();
+            break;
           default:
             appErrors.showIncorrectArgsError();
             break;
@@ -26,7 +61,13 @@ class OSOperations {
       }
     } catch (err) {
       if (err) {
-        appErrors.showOperationError();
+        if (args[0] === "--username") {
+          this.writable.write(
+            "Operation failed! File manager can't find username in this operation system.\n"
+          );
+        } else {
+          appErrors.showOperationError();
+        }
       }
     }
   }
